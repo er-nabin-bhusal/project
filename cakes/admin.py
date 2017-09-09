8faf16a5a3c74408210ec0a4baa9fd3bef4d8aee
nabinbhusal80 <nabinbhusal80@gmail.com>
2017-09-09 10:42:30 +0545

    change in admin

--- cakes/admin.py
+++ cakes/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Cake,CustomCake,OrderCake
 
 # Register your models here.
-admin.site.register(Cake)
+
+class CakeAdmin(admin.ModelAdmin):
+
+	list_display = ['name','price','cake_type','flavour','weight']
+	list_display_links = ['name']
+	list_editable = []
+	list_filter = ['timestamp']
+	search_fields = ['name']
+	class meta:
+		model = Cake
+
+admin.site.register(Cake,CakeAdmin)
+
 admin.site.register(CustomCake)
 admin.site.register(OrderCake)
