a9f8de288653834649a98fa9fce3447171d654f3
nabinbhusal80 <nabinbhusal80@gmail.com>
2017-09-09 11:10:20 +0545

    admin edit

--- cakes/admin.py
+++ cakes/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 from .models import Cake,CustomCake,OrderCake
-
+from django.db.models.functions import Lower
 # Register your models here.
-
 class CakeAdmin(admin.ModelAdmin):
 
 	list_display = ['name','price','cake_type','flavour','weight']
 	list_display_links = ['name']
-	list_editable = []
+	list_editable = ['price']
 	list_filter = ['timestamp']
 	search_fields = ['name']
+	ordering = ['name']
+	def get_ordering(self, request):
+		return [Lower('name')]
 	class meta:
 		model = Cake
 
 admin.site.register(Cake,CakeAdmin)
-
 admin.site.register(CustomCake)
 admin.site.register(OrderCake)
